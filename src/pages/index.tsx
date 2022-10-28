4c48285c5f1434788d24e2a0f6167040ee1d019a
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-10-28 14:22:20 +0200

    added banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,17 @@
 import React from "react";
-import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-
-import styles from "./index.module.css";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="docs/introduction">
-            <h1 className="hero__title">{siteConfig.title}</h1>
-            <p className="hero__subtitle">{siteConfig.tagline}</p>
-          </Link>
-        </div>
-      </div>
-    </header>
+    <Link to="docs/introduction">
+      <img src={useBaseUrl("/img/banner.png")} />;
+    </Link>
   );
 }
 
