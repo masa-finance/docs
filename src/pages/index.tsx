b6ed9cb927481e9691ccd4d51d30aaee09d7e0f8
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-11-16 18:29:02 +0100

    added masa-react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import "@masa-finance/masa-react/dist/style.css";
 import React from "react";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import { useMasa } from "@masa-finance/masa-react";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -17,6 +19,8 @@ function HomepageHeader() {
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
+  const { connect } = useMasa();
+
   return (
     <Layout
       title={`${siteConfig.title}`}
@@ -24,6 +28,7 @@ export default function Home(): JSX.Element {
     >
       <HomepageHeader />
       <main>
+        <button onClick={() => connect!()}>masa masa ho</button>
         <HomepageFeatures />
       </main>
     </Layout>
