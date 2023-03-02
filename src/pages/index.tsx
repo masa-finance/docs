33658de06b0843c986678d2ff25d740e9394945a
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-03-02 11:00:21 +0100

    fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
-import "@masa-finance/masa-react/dist/src/provider/styles.scss";
-
 import React from "react";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import { MasaProvider } from "@masa-finance/masa-react";
 import Hero from "../components/Hero";
-import MasaLogin from "@site/src/components/masa-login/masa-login";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 function HomepageHeader() {
   return (
     <Link to="docs/masa/introduction">
-      <Hero />
+      <Hero/>
     </Link>
   );
 }
@@ -20,14 +17,26 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <MasaProvider>
-      <Layout
-        title={`${siteConfig.title}`}
-        description="Description will go into a meta tag in <head />"
-      >
-        <MasaLogin />
-        <HomepageHeader />
-      </Layout>
-    </MasaProvider>
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <BrowserOnly>
+        {() => {
+          require("@masa-finance/masa-react/dist/src/provider/styles.scss");
+
+          const { MasaProvider } = require("@masa-finance/masa-react");
+          const MasaLogin = require("@site/src/components/masa-login/masa-login");
+
+          return (
+            <MasaProvider>
+              <MasaLogin/>
+            </MasaProvider>
+          );
+        }}
+      </BrowserOnly>
+
+      <HomepageHeader/>
+    </Layout>
   );
 }
