74801121c4d76a909c42fe958159ee861c506bdb
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-04-04 13:37:47 -0700

    Fix build issue relating to `Link` object in index.tsx by adding react-router-dom dependency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import Hero from "../components/Hero";
